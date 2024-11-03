79ad7417e538523371747de6de00cc7f888ab979
lapjon <jonlaprevote@hotmail.com>
2024-11-03 17:45:04 +0100

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,56 @@
+package org.example;
+
+
+
+public class Calculator {
+
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public double multiply(int a, int b){
+        return a * b;
+    }
+
+    public double divide(int a, int b){
+
+        if (b == 0){
+            throw new IllegalArgumentException("Can't divide with 0");
+        }
+        else {
+            return (double) a / (double) b;
+        }
+    }
+
+
+
+    public double squareRootOf(int a) {
+        return Math.sqrt(a);
+    }
+
+
+    public double getArea(int a) {
+        if (a < 0) {
+            return Double.NaN;
+        }
+        double area = Math.PI * (a * a);
+        return Math.round(area * 100.0) / 100.0;
+    }
+
+
+    public double getCircumference(int a) {
+        if (a < 0) {
+            return Double.NaN;
+        }
+        double circumference = Math.PI * 2 * a;
+        return Math.round(circumference * 100.0) / 100.0;
+    }
+}
+
+
